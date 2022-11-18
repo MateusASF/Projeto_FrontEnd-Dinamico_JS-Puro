f4bc3453fffab724c718b8b2a52030780e108613
Mateus Augusto dos Santos Fonseca <mateus.asfonseca@gmail.com>
2022-11-17 23:50:17 -0300

    conexão com API realizada porém footer não aparece

--- Mateus/arquivo.js
+++ Mateus/arquivo.js
@@ -17,11 +17,34 @@
 
         main.appendChild(
             biblioteca.footer([
-                biblioteca.elementoFooter({
-                    nomeCategoria: 'teste',
-                    quantidadeCategoria: 5,
-                    linkA: '../index.html'
-                }),
+                // fetch('http://estabelecimentos.letscode.dev.netuno.org:25390/services/category/list', {
+                //     method: 'POST',
+                //     headers: {
+                //         "Content-Type": "application/json"
+                //     },
+                //     body: JSON.stringify({
+                //         text: 'string',
+                //         group: {
+                //             uid: '1a7fba04-cc35-4ded-b0ab-fdfcfd649df2'
+                //         }
+                //     })
+                // }).then((response) => {
+                //     if (response.ok) {
+                //         // for (child of response) {
+                //         // }
+                //         alert('sou lindo')
+
+                //         response.json().then(() => {
+
+                //         })
+
+
+                //     } else {
+                //         alert('caiu aqui no else')
+                //     }
+                // }).catch((error) => {
+                //     alert('Erro geral na comunicação:')
+                // })
                 biblioteca.elementoFooter({
                     nomeCategoria: 'teste',
                     quantidadeCategoria: 5,
